b3f832fa130f9b78b487e6785dc216b80b584ff7
ElizabethPeredo <61327189+ElizabethPeredo@users.noreply.github.com>
2021-12-29 01:31:21 -0500

    Index.html

---
+++ assets/js/tailwind.js
@@ -0,0 +1,29 @@
+tailwind.config = {
+    theme: {
+        container: {
+            center: true,
+            padding: '1rem',
+        },
+        extend: {
+            colors: {
+                color1: '#1cb0f6',
+                color2: '#ffc800',
+                color3: '#e5e5e5',
+                color4: '#ff4b4b',
+                color5: '#afafaf',
+
+            },
+            rotate: {
+                '360': '360deg',
+            },
+            fontFamily: {
+                title: "'Rubik', cursive",
+                subtitle: "'Poppins', cursive",
+                body: "'Poppins', sans- serif",
+                banner: "'Josefin Sans', cursive",
+                logo: "'Rammetto One', cursive",
+                code: "'Fira Code', monospace",
+            }
+        }
+    }
+}
